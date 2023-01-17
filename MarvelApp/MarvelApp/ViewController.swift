66d50d911f5250d299bcc818aa5e17420f62d80f
Georgiy <33875528+georg1856@users.noreply.github.com>
2023-01-17 15:46:05 +1000

    configure VC

--- MarvelApp/MarvelApp/ViewController.swift
+++ MarvelApp/MarvelApp/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let urlMarvel = "https://gateway.marvel.com/v1/public/characters?ts=10&apikey=31a63b3b088f1225ef9e5d5f56a97b85&hash=070548dfe73f5680972208e2cb02f1f7"
+    @IBOutlet weak var tableView: UITableView!
+    var marvel: [Result] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureTableView()
     }
-
-
+    
+    func configureTableView() {
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 44
+        
+        self.tableView.register(UINib(nibName: "MarvelTableViewCell", bundle: nil),
+                                forCellReuseIdentifier: "MarvelTableViewCell")
+    }
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.marvel.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "MarvelTableViewCell", for: indexPath) as? MarvelTableViewCell {
+            cell.marvelCharacter = self.marvel[indexPath.row]
+            return cell
+        }
+        return UITableViewCell()
+    }
+}
