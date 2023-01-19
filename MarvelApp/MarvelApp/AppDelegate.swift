9bded911a4d7b83e3d5826f9e42924c74e1bfec9
Georgiy <33875528+georg1856@users.noreply.github.com>
2023-01-19 17:35:47 +1000

    Bug-fix design

--- MarvelApp/MarvelApp/AppDelegate.swift
+++ MarvelApp/MarvelApp/AppDelegate.swift
@@ -12,13 +12,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    //func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//
+//        window = UIWindow(frame: UIScreen.main.bounds)
+//        window?.makeKeyAndVisible()
+//        window?.backgroundColor = .systemBackground
+//        window?.rootViewController = MarvelVC()
+//
+//        return true
         
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.makeKeyAndVisible()
-        window?.backgroundColor = .systemBackground
-        window?.rootViewController = MarvelVC()
         
-        return true
-    }
+        
+        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+            
+            let navigationController = UINavigationController.init(rootViewController: MarvelVC())
+            self.window = UIWindow.init(frame: UIScreen.main.bounds)
+            self.window?.rootViewController = navigationController
+            self.window?.makeKeyAndVisible()
+            let navigationBarAppearance = UINavigationBarAppearance()
+            navigationBarAppearance.backgroundColor = .white
+            navigationController.navigationBar.scrollEdgeAppearance = navigationBarAppearance
+            navigationController.navigationBar.prefersLargeTitles = true
+            
+            return true
+        }
 }
